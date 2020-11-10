6338a7adb2bdaa5693d0f077bcd6ea60d1b5f928
Gabby Vicas <gabbyvicas@Gabbys-MacBook-Pro.local>
2020-11-09 21:11:45 -0600

    added episodes test

--- src/api/fetchShow.js
+++ src/api/fetchShow.js
@@ -3,14 +3,12 @@ import axios from 'axios'
 
 export const fetchShow = () => {
     return axios
-    .get(
-        "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-      )
+    .get("https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes")
       .then(res => {
-          return res})
+          return res
+        })
         .catch(err => {
             console.log("error", err)
             return err
         })
-  }
-    
+    }
